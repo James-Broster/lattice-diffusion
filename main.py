e0625d669972d4c5f19debfe0a3544f3bf6d99a8
Ronald Cvek <72977876+roncv@users.noreply.github.com>
2023-11-23 15:23:45 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+import matplotlib.pyplot as plt
+
+# Define the system dimensions
+width = 10
+height = 10
+
+# Define the initial concentrations
+concentrations = [[0.0 for _ in range(width)] for _ in range(height)]
+concentrations[4][4] = 1.0
+
+# Define the AgentBasedSystem class
 class Agent:
     def __init__(self, concentration):
         self.concentration = concentration
@@ -41,3 +52,21 @@ class AgentBasedSystem:
                 new_grid[i][j] = new_agent
 
         self.grid = new_grid
+
+# Create the agent-based system
+system = AgentBasedSystem(width, height)
+system.initialize_agents(concentrations)
+
+# Visualize the initial state
+plt.imshow(concentrations, cmap='hot', interpolation='nearest')
+plt.colorbar()
+plt.title('Initial Concentrations')
+plt.show()
+
+# Perform diffusion and visualize the final state
+system.diffuse()
+final_concentrations = [[agent.concentration for agent in row] for row in system.grid]
+plt.imshow(final_concentrations, cmap='hot', interpolation='nearest')
+plt.colorbar()
+plt.title('Final Concentrations')
+plt.show()
